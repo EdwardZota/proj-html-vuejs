ab68d4ab4b7e305b94371fae4b88f0b19865f621
Edward Zota <zotaedward@gmail.com>
2023-04-13 09:36:32 +0200

    esercizio concluso(con aggiunta di diversi bonus personalizzati)

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -4,6 +4,21 @@ export default {
     props: {
         HeaderNav: Array,
         HeaderRight: Array
+    },
+    data(){
+        return{
+            searchBar:false
+        }
+    },
+    methods:{
+        showSearchBar(){
+            if(this.searchBar){
+                this.searchBar=false;
+            }else{
+                this.searchBar=true;
+            }
+
+        }
     }
 }
 
@@ -31,7 +46,11 @@ export default {
             <nav class="RightNav">
                 <ul>
                     <li>
-                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <div class="searchbar" v-if="searchBar">
+                            <input type="text" @keyup.enter="showSearchBar" placeholder="Ricerca">
+
+                        </div>
+                        <i class="fa-solid fa-magnifying-glass" @click="showSearchBar" v-if="!searchBar"></i>
                     </li>
                     <li v-for="bottone in HeaderRight">
                         <button>
@@ -46,33 +65,26 @@ export default {
 
 <style lang="scss" scoped>
 @use '../styles/partials/variable';
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-.dropdown-content {
-    display: none;
+
+.searchbar{
     position: absolute;
-    top: 20px;
-    left: -60px;
-    min-width: 100px;
-    z-index: 1;
-    background-color: variable.$header-shadow-btn;
-}
-
-.dropdown-content a {
-    color: variable.$header-color-text-btn;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-.element:hover .dropdown-content{
-    display: block;
-}
-
-.dropdown-content a:hover {
-    background-color: variable.$header-color-text-btn;
-    color: variable.$header-shadow-btn;
+    top: 0;
+    right: 0px;
+    width: 180px;
+    overflow: hidden;
+    input{
+        width: 100%;
+        padding: 5px 10px;
+        border: none;
+        color: variable.$header-color-text-btn;
+        background-color: variable.$header-bg-btn;
+        box-shadow: variable.$header-shadow-btn;
+        border-radius: 10px;
+        outline: none;
+        &::placeholder{
+            color: variable.$header-color-text-btn;
+        }
+    }
 }
 
 header {
@@ -116,10 +128,16 @@ li {
 .RightNav ul li:first-child {
     line-height: 32px;
     margin-right: 10px;
-    color: gray;
+    color: variable.$text-color-primary;
+    position: absolute;
+    top: 0;
+    left: -25px;
+    
 }
 
-.RightNav button {
+.RightNav{
+    position: relative;
+    button {
     background-color: variable.$header-bg-btn;
     color: variable.$header-color-text-btn;
     border: 1px solid #2f55d41a;
@@ -133,6 +151,44 @@ li {
         color: variable.$header-shadow-btn;
     }
 }
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+.dropdown-content {
+    display: none;
+    position: absolute;
+    top: 20px;
+    left: -60px;
+    min-width: 100px;
+    z-index: 1;
+    background-color: variable.$header-shadow-btn;
+}
+
+.dropdown-content a {
+    color: variable.$header-color-text-btn;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+.element:hover .dropdown-content{
+    display: block;
+}
+
+.dropdown-content a:hover {
+    background-color: variable.$header-color-text-btn;
+    color: variable.$header-shadow-btn;
+}
+
+@media screen and (max-width:1200px) {
+    .searchbar,.fa-solid.fa-magnifying-glass{
+        display: none;
+    }
+    
+}
+
 @media screen and (max-width:768px) {
     .RightNav{
         display: none;
